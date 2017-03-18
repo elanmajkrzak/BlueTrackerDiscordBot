2df883f9306bdc6a0de672a6dee3057f5f175685
Elan Majkrzak <elanmajkrzak@gmail.com>
2017-03-17 20:44:08 -0500

    bot properly outputs title and links of all currents blog posts on playoverwatch when users input into bot's channel

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const config = require('./config.json');
 const Discord = require("discord.js");
-const scraper = require("scraper.js")
+const request = require('request');
+const cheerio = require('cheerio');
 const client = new Discord.Client();
 
 client.login(config.token);
@@ -10,7 +11,36 @@ client.on('ready', () => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.sendMessage("pong!");
+  if (message.content.startsWith("!update")) {
+	url = 'https://playoverwatch.com/en-us/blog/'
+
+	request(url, function(error, response, html){
+        var json = { title : "", link : "", date : ""};
+        if(!error){
+        	var $ = cheerio.load(html);
+            var title, link, date;
+
+            $('.blog-details').filter(function(){
+            	var posts = $(this);
+
+            	json.title = posts.children().first().attr('title');
+
+            	var link = posts.children().first().attr('href');
+
+            	if (link.search('^http') != -1) {
+            		json.link = link;
+            	} else {
+            		json.link = 'https://playoverwatch.com' + link;
+            	}
+            	//json.date = posts.next().hasClass('sub-title').text();
+
+            	console.log(json.title + " " + json.link);
+    			message.channel.sendMessage(json.title + " " + json.link);
+            });
+        }
+        else {
+        	console.log(error);
+        }
+    })
   }
 });
